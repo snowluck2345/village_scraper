b0f4af010f75d58af9637f6eac8f7ee41d092c59
snowluck2345 <snowluck2345@gmail.com>
2015-07-11 21:20:40 -0700

    matlab export added
    matlab export added

--- test.py
+++ test.py
@@ -5,8 +5,13 @@ from bs4 import BeautifulSoup
 
 with Browser() as browser:
     # Visit URL
-    url = "http://www.twstats.com/en81/index.php"
+    url = "http://www.twstats.com/en81/index.php?page=village_locator&stage=2&source=village"
     browser.visit(url)
+    aaa = browser.find_by_name('village_coords')
+    browser.fill(aaa, '440|429')
+    browser.find_by_css('.ui-button.ui-widget.ui-state-default.ui-corner-all').click()
+    
+
     html_doc = browser.html
     soup = BeautifulSoup(html_doc, 'html.parser')
 
--- village_scraper_twstats.py
+++ village_scraper_twstats.py
@@ -1,8 +1,9 @@
-import csv
 from bs4 import BeautifulSoup
 import re
 import scipy.io as sio
-import re
+import numpy as np
+
+#mat_village_file = sci.loadmat('list.mat')
 
 soup = BeautifulSoup(open("TWStatsTest3.html"))
 
@@ -18,14 +19,14 @@ sub_table = table.tr.contents[3]
 tbody = sub_table.div.table
 contents = tbody.contents
 
-villages = [None] * 135
+villages = [None] * 200
 for i in range(135):
-  	villages[i] = [None] * 4
+  	villages[i] = [None] * 3
 
-for n in range (3, 100, 2):
+for n in range (3, 273, 2):
 	
 
-#	print n
+	print n
 
 
 	distance = contents[n].contents[1]
@@ -44,7 +45,7 @@ for n in range (3, 100, 2):
 	#print a
 
 	#print distance.string
-	villages[a][0] = float(distance.string)
+	#villages[a][0] = float(distance.string)
 	#print villages[a][0]
 	temp_string = name_and_coordinates.string
 	bob = re.split("\(", temp_string)
@@ -52,42 +53,51 @@ for n in range (3, 100, 2):
 	bob3 = re.split("\|", bob2[0])
 #	print bob3[0]
 #	print bob3[1]
-	villages[a][1] = bob3[0]
-	villages[a][2] = bob3[1]
+	villages[a][0] = int(bob3[0])
+	villages[a][1] = int(bob3[1])
 #	print villages[a][1]
 #	print villages[a][2]
 	#print villages[a][1]
-	villages[a][3] = int(re.sub("[^\d\.]", "", points.string))
+	villages[a][2] = int(re.sub("[^\d\.]", "", points.string))
 	#print villages[a][2]
 
 
-valid_villages = [None] * 135
+valid_villages = [None] * 200
 for i in range(135):
-  	valid_villages[i] = [None] * 4
+  	valid_villages[i] = [None] * 3
 
 k = 0
-for n in range (0, 134):
-	if villages[n][3] < 500 and villages[n][0] >= 0 and villages[n][3] > 0 and villages[n][0] <= 6 :
-		for m in range (0, 4):
+for n in range (0, 135):
+	if villages[n][2] < 500 and villages[n][2] > 0 :
+		for m in range (0, 3):
 			valid_villages[k][m] = villages[n][m]
 
 			#print m
-#		print "---------"
-#		print k
-#		print "---------"
-#		print valid_villages[k][0]
-#		print valid_villages[k][1]
-#		print valid_villages[k][2]
+		print "---------"
+		print k
+		print "---------"
+		print valid_villages[k][0]
+		print valid_villages[k][1]
+		print valid_villages[k][2]
 #		print valid_villages[k][3]
 		k = k + 1 
 
 #print "-------"
-print k-1
+#print k-1
 #print valid_villages
 #print villages
 
+villages = filter(None, villages)
+
+print '===='
+
+info = np.array(villages)
+
+print info
 
 
+
+sio.savemat('clucka.mat', {'test':info})
 
 
 
